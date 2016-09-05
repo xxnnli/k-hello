b7761c22bc0bb5a7c91dc5c8de9430840be4da81
Peter Li <xli@thesearchmonitor.com>
2016-09-05 16:01:44 -0500

    add at controllter

--- app/Http/Controllers/QbController.php
+++ app/Http/Controllers/QbController.php
@@ -21,6 +21,19 @@ class QbController extends Controller
         $req_url = 'https://oauth.intuit.com/oauth/v1/get_request_token';
         $auth_url = 'https://appcenter.intuit.com/Connect/Begin';
         $rt = $this->oauth->getRequestToken($req_url, str_replace('/rt', '/at', $request->url()));
+        $request->session()->put('secret', $rt['oauth_token_secret']);
         return redirect($auth_url .'?oauth_token='.$rt['oauth_token']);
+    }
+
+    public function at(Request $request)
+    {
+        $acc_url = 'https://oauth.intuit.com/oauth/v1/get_access_token';
+        $secret = $request->session()->get('secret');
+        $this->oauth->setToken($request->input('oauth_token'), $secret);
+        $at = $this->oauth->getAccessToken($acc_url);
+        $token = serialize($at);
+        $realmId = $request->input('realmId');  // realmId is legacy for customerId;
+        $dataSource = $request->input('dataSource');
+        return view('qb_token_result', ['secret'=>$secret, 'token'=>$token, 'realmId'=>$realmId, 'dataSource'=>$dataSource]);
     }
 }
---
+++ resources/views/qb_token_result.php
@@ -0,0 +1,10 @@
+<html>
+    <body>
+        <ul>
+            <li>secret: {{$secret}}</li>
+            <li>token: {{$token}}</li>
+            <li>realmId: {{$realmId}}</li>
+            <li>dataSource: {{$dataSource}}</li>
+        </ul>
+    </body>
+</html>
