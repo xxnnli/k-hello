09118bc3b5e0572357399ccae5b78011376d42d7
Peter Li <xli@thesearchmonitor.com>
2016-09-05 13:02:50 -0500

    add exit after header redirect

--- app/Http/Controllers/QbController.php
+++ app/Http/Controllers/QbController.php
@@ -10,5 +10,6 @@ class QbController extends Controller
         $auth_ulr = 'https://appcenter.intuit.com/Connect/Begin';
         $rt = '111111';
         header('Location: '. $auth_ulr .'?oauth_token='.$rt);
+        exit;
     }
 }
